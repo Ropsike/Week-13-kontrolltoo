51915499707a5ba71f17cc83ef5a3b37851202ac
Opilane TTHK <opilane@tthk.ee>
2021-11-24 09:38:13 +0200

    rawr

---
+++ Ülesanne 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Ülesanne_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples\test";
+            string userToyota = "Toyota";
+            string userBmw = "Bmw";
+            string userTesla = "Tesla";
+
+
+            string fullToyotaPath = $@"{rootDirectory}\{userToyota}.txt";
+            string fullBmwPath = $@"{rootDirectory}\{userBmw}.txt";
+            string fullTeslaPath = $@"{rootDirectory}\{userTesla}.txt";
+
+            bool directoryExists = Directory.Exists(rootDirectory);
+            bool fileToyotaExists = File.Exists(fullToyotaPath);
+            bool fileBmwExists = File.Exists(fullBmwPath);
+            bool fileTeslaExists = File.Exists(fullTeslaPath);
+
+            if (directoryExists && fileToyotaExists && fileBmwExists && fileTeslaExists)
+            {
+                Console.WriteLine($"{userToyota} and {userBmw} and {userTesla} already exists in {rootDirectory}");
+            }
+            else if (!directoryExists)
+            {
+                Console.WriteLine($"{rootDirectory} does not exist.");
+                Directory.CreateDirectory(rootDirectory);
+                Console.WriteLine($"{rootDirectory} has been created.");
+                File.Create(fullToyotaPath);
+                File.Create(fullBmwPath);
+                File.Create(fullTeslaPath);
+                Console.WriteLine($"{userToyota} and {userBmw} and {userTesla} already exists in {rootDirectory}");
+
+            }
+            else
+            {
+                File.Create(fullToyotaPath);
+                File.Create(fullBmwPath);
+                File.Create(fullTeslaPath);
+                
+                Console.WriteLine($"{userToyota} and {userBmw} and {userTesla} has been created in {rootDirectory}");
+            }
+        }
+    }
+}
---
+++ Ülesanne 3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Ülesanne_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootdirect = @"C:\Users\opilane\Samples\";
+
+            string[] datafromfile = File.ReadAllLines($@"{rootdirect}\cars.txt");
+            List<string> Tesla = new List<string>();
+            List<string> Toyota = new List<string>();
+            List<string> bmw = new List<string>();
+
+            foreach (string element in datafromfile)
+            {
+                if (element.Contains("Toyota"))
+                {
+                    Toyota.Add(element);
+                }
+                if (element.Contains("BMW"))
+                {
+                    bmw.Add(element);
+                }
+                else if (element.Contains("Tesla"))
+                {
+                    Tesla.Add(element);
+                }
+
+                File.WriteAllLines($@"{rootdirect}\Toyota.txt", Toyota);
+                File.WriteAllLines($@"{rootdirect}\Bmw.txt", bmw);
+                File.WriteAllLines($@"{rootdirect}\Tesla.txt", Tesla);
+            }
+
+
+
+
+
+        }
+    }
+
+}
